8be14a1d4bbd21804f04a497f7f466a9f2981c64
OS User <kiran.sridhar@emc.com>
2017-03-05 23:39:38 +0530

    Added_More
    >Creation of bootable volume
    >Instance from bootable volume
    >snapshot creation
    > snapshot deletion

--- tests.py
+++ tests.py
@@ -12,6 +12,7 @@ import re
 # This will be the inputs & variable initilizations
 volume_name = sys.argv[1]
 server_name = sys.argv[2]
+volume_name_bootable = volume_name + "_" + "bootable"
 print "volume & server" , volume_name , server_name
 o_chdir = os.chdir("/opt/stack/devstack")
 size_vol = 5
@@ -22,7 +23,9 @@ no_attach = []
 replication_status = None
 volume_available_string = 'available'
 server_available_string = 'ACTIVE'
+bootable_string = 'true'
 new_volume_size = size_vol +  10
+server_from_bootable_volume = 'server_from_bootable_volume'
 
 # function to get the latest status of the volume
 def latest_volume_status(volume_name):
@@ -197,29 +200,123 @@ def check_pattern_match(pattern,input_string):
 
 
 # # print "\n\n================VOLUME EXTEND================\n\n"
-print "New size is" , new_volume_size
-list_check_output = ['openstack' , 'volume' , 'set' , '--size' , str(new_volume_size), volume_name]
-op_extend_vol = subprocess.check_output(list_check_output)
+# print "New size is" , new_volume_size
+# list_check_output = ['openstack' , 'volume' , 'set' , '--size' , str(new_volume_size), volume_name]
+# op_extend_vol = subprocess.check_output(list_check_output)
+#
+# ## check for extension of volume
+# op = subprocess.check_output(['openstack' , 'volume' , 'show', volume_name , '-f', 'json'])
+# op = yaml.load(op)
+#
+# print "NEW SIZE FROM OP" , op['size']
+#
+# # print "\n\n================VOLUME CLONING================\n\n"
+# list_check_output = ['openstack' , 'volume' , 'create' , '--source' , volume_name, cloned_volume_name]
+# op_extend_vol = subprocess.check_output(list_check_output)
+#
+# # Check size, new volume name, old volume name, bootable,status,replication_status
+#
+# # print "\n\n================VOLUME SNAPSHOT================\n\n"
+# snapshot_volume_name = "snapshot" + "_" + volume_name
+# list_check_output = ['openstack' , 'volume' ,'snapshot' ,'create' , "--volume" ,volume_name , snapshot_volume_name]
+# op_snaps_vol_create = subprocess.check_output(list_check_output)
 
-## check for extension of volume
-op = subprocess.check_output(['openstack' , 'volume' , 'show', volume_name , '-f', 'json'])
-op = yaml.load(op)
+# # print "\n\n================VOLUME SNAPSHOT CREATION CHECK================\n\n"
+# #openstack volume snapshot show snap_qe_vol_silver_99 -f  json
+# list_check_output = ['openstack' , 'volume' ,'snapshot' ,'show' , snapshot_volume_name , '-f', 'json']
+# op_snaps_vol_show = subprocess.check_output(list_check_output)
+# print "Type of snap output is before yaml" , type(op_snaps_vol_show)
+#
+# op_snaps_vol_show = yaml.load(op_snaps_vol_show)
+#
+# print "Type of snap output is" , type(op_snaps_vol_show)
+#
+# # need a delay as it shows "creating" state
+# print "From output ", op_snaps_vol_show['name'] , op_snaps_vol_show['volume_id'] , op_snaps_vol_show['status']
+#
+#
+# # print "\n\n================VOLUME SNAPSHOT DELETION================\n\n"
+# list_check_output = ['openstack' , 'volume' ,'delete' ,snapshot_volume_name]
+#
+# op_snaps_vol_show = yaml.load(op_snaps_vol_show)
+# need a delay as it shows "deleting" state
+## Expected ouput check is for "No snapshot with a name or ID of 'snappy2' exists."
 
-print "NEW SIZE FROM OP" , op['size']
+# # Check snapshot name , volume id from which the snapshot was taken, status, size, os-extended-snapshot-attributes
+#
+# # print "\n\n================VOLUME CLONING FROM SNAPSHOT================\n\n"
+# list_check_output = ['openstack' , 'volume' , 'create' , '--source' , snapshot_volume_name, cloned_volume_name]
 
-# print "\n\n================VOLUME CLONING================\n\n"
-list_check_output = ['openstack' , 'volume' , 'create' , '--source' , volume_name, cloned_volume_name]
-op_extend_vol = subprocess.check_output(list_check_output)
+print "\n\n================CREATING VOLUME FROM IMAGE================\n\n"
+##openstack volume create --image cirros-0.3.4-x86_64-uec --type VMAX_SILVER --size 20 qe_neither_bootable_1
 
-# Check size, new volume name, old volume name, bootable,status,replication_status
+# Get the images dynamically
+list_check_output = ['openstack' , 'image' , 'list' , '-f' , 'json']
+op_image_list = subprocess.check_output(list_check_output)
 
-# print "\n\n================VOLUME SNAPSHOT================\n\n"
-snapshot_volume_name = "snapshot" + volume_name
-list_check_output = ['openstack' , 'volume' ,'snapshot' ,'create' , '--volume' , volume_name, snapshot_volume_name]
-op_extend_vol = subprocess.check_output(list_check_output)
+##** This conversion to yaml is important as it gives the type of theouput in list format. From json output, by default, op_image_list will ocme out as a string :(
+op_image_list = yaml.load(op_image_list)
+print "\nIMAGE TO BE USED IS" , op_image_list[0]['Name']
 
-# Check snapshot name , volume id from which the snapshot was taken, status, size, os-extended-snapshot-attributes
+list_check_output = ['openstack' , 'volume' , 'create' , '--image' , op_image_list[0]['Name'] , '--type' , type_vol , '--size', str(size_vol) , volume_name_bootable]
+op_bootable_volume_create = subprocess.check_output(list_check_output)
+op_bootable_volume_create = yaml.load(op_bootable_volume_create)
 
-# print "\n\n================VOLUME CLONING FROM SNAPSHOT================\n\n"
-list_check_output = ['openstack' , 'volume' , 'create' , '--source' , volume_name, cloned_volume_name]
+print "\n\n================CHECKING FOR THE CREATION OF BOOTABLE VOLUME================\n\n"
+o_chdir = os.chdir("/opt/stack/devstack")
+op_bootable_volume_show = latest_volume_status(volume_name_bootable)
+# op_bootable_volume_show = yaml.load(op_bootable_volume_show)
 
+print "\n\nop_bootable_volume_show is" , op_bootable_volume_show
+
+# bootable non-attached volume show
+while (op_bootable_volume_show['status'] != 'available'):
+    if (op_bootable_volume_show['status'].lower == 'error'):
+        print "\nFAILURE IN CREATING VOLUME %s. EXITING" % (op_bootable_volume_show['name'])
+        break
+    print "\nWAITING FOR STATUS OF THE VOLUME %s TO BE AVAILABLE. CURRENTLY VOLUME STATE IS IN %s\n" % (op_bootable_volume_show['name'],op_bootable_volume_show['status'])
+    time.sleep(10)
+    op_bootable_volume_show = latest_volume_status(volume_name_bootable)
+
+inputs = [volume_name_bootable,size_vol,type_vol,no_attach,replication_status,volume_available_string, bootable_string]
+print "\nINPUTS TO CREATE VOLUME" , inputs
+
+values = [op_bootable_volume_show['name'],op_bootable_volume_show['size'],op_bootable_volume_show['type'],op_bootable_volume_show['attachments'],op_bootable_volume_show['replication_status'],op_bootable_volume_show['status'],op_bootable_volume_show['bootable']]
+print "\nVALUES FROM THE CREATED VOLUME", values
+
+if values == inputs :
+    print "\nBOOTABLE VOLUME CREATED SUCCESSFULLY\n"
+
+print "\n\n================CREATING AN INSTANCE FROM BOOTABLE VOLUME================\n\n"
+# openstack server create --volume qe_vol_silver_19991_bootable --flavor m1.tiny inst_vol
+list_check_output = ['openstack' , 'server' , 'create' , '--volume' , volume_name_bootable , '--flavor' , "m1.tiny", server_from_bootable_volume]
+op_server_create = subprocess.check_output(list_check_output)
+
+list_check_output = ['openstack' , 'server' , 'show',  server_from_bootable_volume, "-f", "json"]
+op_server_show = subprocess.check_output(list_check_output)
+op_server_show = yaml.load(op_server_show)
+
+print "\n\nop_server_show is" , op_server_show
+print "\n\nTYPE OF op_server_show" , type(op_server_show)
+
+print "EXTRACTABLE STUFF" , op_server_show['id'], op_server_show['name'], op_server_show['status'] , op_server_show['volumes_attached']
+
+#
+#
+# print "\n\n================DELETION OF BOOTABLE VOLUME================\n\n"
+# o_chdir = os.chdir("/opt/stack/devstack")
+# list_checkOutput_delete = ['openstack' ,'volume' ,'delete' ,  volume_name_bootable]
+# op = subprocess.check_output(list_checkOutput_delete)
+#
+# op = latest_volume_status(volume_name_bootable)
+#
+# # Enter only if the volume exists
+# if op != 1:
+#     while (op['status'] == 'deleting'):
+#         print "\nWAITING FOR VOLUME %s TO BE DELETED. CURRENTLY VOLUME STATE IS IN %s\n" % (op['name'], op['status'])
+#         time.sleep(10)
+#         op = latest_volume_status(volume_name_bootable)
+#         if op == 1:
+#             break
+#             print "CURRENT STATUS OF VOLUME IS" , op , "HENCE CONTINUING"
+#
