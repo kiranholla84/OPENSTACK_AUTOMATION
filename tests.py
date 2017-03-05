1bcafe7b60a5f45669762eedffe3c8a7324cc431
OS User <kiran.sridhar@emc.com>
2017-03-05 12:30:23 +0530

    Change as of 4th April
    4th April
    -added extend
    -Clone from clone

--- tests.py
+++ tests.py
@@ -14,7 +14,7 @@ volume_name = sys.argv[1]
 server_name = sys.argv[2]
 print "volume & server" , volume_name , server_name
 o_chdir = os.chdir("/opt/stack/devstack")
-size_vol = 1
+size_vol = 5
 type_vol = 'VMAX_SILVER'
 image = 'cirros-0.3.4-x86_64-uec' #Keep a function here to get the value automatically.
 flavor = 'm1.tiny'
@@ -22,6 +22,7 @@ no_attach = []
 replication_status = None
 volume_available_string = 'available'
 server_available_string = 'ACTIVE'
+new_volume_size = size_vol +  10
 
 # function to get the latest status of the volume
 def latest_volume_status(volume_name):
@@ -146,13 +147,79 @@ def check_pattern_match(pattern,input_string):
 #             break
 #         print "CURRENT STATUS OF SERVER IS" , op , "HENCE CONTINUING"
 
+# print "\n\n================VOLUME ATTACH================\n\n"
 # volume addition to server or volume attach
-list_check_output = ['openstack' , 'server' , 'add' , 'volume' , server_name, volume_name]
-op_attach_vol = subprocess.check_output(list_check_output)
+# list_check_output = ['openstack' , 'server' , 'add' , 'volume' , server_name, volume_name]
+# op_attach_vol = subprocess.check_output(list_check_output)
 
-# check for addition of volume to server
+# # check for addition of volume to server
+# op = subprocess.check_output(['openstack' , 'volume' , 'show', volume_name , '-f', 'json'])
+# op = yaml.load(op)
+#
+# print "op is" , op
+# attachment_details_server_id = op['attachments'][0]['server_id']
+# attachment_details_attachment_id = op['attachments'][0]['attachment_id']
+# attachment_details_volume_id = op['attachments'][0]['volume_id']
+# attachment_details_device = op['attachments'][0]['device']
+#
+# print "attachment details" , attachment_details_server_id, attachment_details_attachment_id,attachment_details_volume_id,attachment_details_device
+
+# inputs = [volume_name,size_vol,type_vol,no_attach,replication_status,volume_available_string]
+# print "\nINPUTS TO CREATE VOLUME" , inputs
+#
+# values = [(op['name']),op['size'],op['type'],op['attachments'],op['replication_status'],op['status']]
+# print "\nVALUES FROM THE CREATED VOLUME", values
+
+# if values == inputs :
+#     print "\nVOLUME ATTACHED SUCCESSFULLY\n"
+#
+# print "\n\n================VOLUME DETACH================\n\n"
+# # volume addition to server or volume attach
+# list_check_output = ['openstack' , 'server' , 'remove' , 'volume' , server_name, volume_name]
+# op_attach_vol = subprocess.check_output(list_check_output)
+
+## check for addition of volume to server
+# op = subprocess.check_output(['openstack' , 'volume' , 'show', volume_name , '-f', 'json'])
+# op = yaml.load(op)
+#
+# print "op is" , op
+# print "\n\n\n"
+# try:
+#     attachment_details_server_id = op['attachments'][0]['server_id']
+#     attachment_details_attachment_id = op['attachments'][0]['attachment_id']
+#
+#     attachment_details_volume_id = op['attachments'][0]['volume_id']
+#     attachment_details_device = op['attachments'][0]['device']
+# except IndexError:
+#     print "There does not seem to be any volume attached to server. Yes!"
+
+# print "attachment details %s :: %s ::  %s :: %s"  %(attachment_details_server_id, attachment_details_attachment_id,attachment_details_volume_id,attachment_details_device)
+
+
+# # print "\n\n================VOLUME EXTEND================\n\n"
+print "New size is" , new_volume_size
+list_check_output = ['openstack' , 'volume' , 'set' , '--size' , str(new_volume_size), volume_name]
+op_extend_vol = subprocess.check_output(list_check_output)
+
+## check for extension of volume
 op = subprocess.check_output(['openstack' , 'volume' , 'show', volume_name , '-f', 'json'])
 op = yaml.load(op)
 
-print "op parameters are", op['status'], op['size'], op['attachments']['server_id'] , op['attachments']['volume_id']
+print "NEW SIZE FROM OP" , op['size']
+
+# print "\n\n================VOLUME CLONING================\n\n"
+list_check_output = ['openstack' , 'volume' , 'create' , '--source' , volume_name, cloned_volume_name]
+op_extend_vol = subprocess.check_output(list_check_output)
+
+# Check size, new volume name, old volume name, bootable,status,replication_status
+
+# print "\n\n================VOLUME SNAPSHOT================\n\n"
+snapshot_volume_name = "snapshot" + volume_name
+list_check_output = ['openstack' , 'volume' ,'snapshot' ,'create' , '--volume' , volume_name, snapshot_volume_name]
+op_extend_vol = subprocess.check_output(list_check_output)
+
+# Check snapshot name , volume id from which the snapshot was taken, status, size, os-extended-snapshot-attributes
+
+# print "\n\n================VOLUME CLONING FROM SNAPSHOT================\n\n"
+list_check_output = ['openstack' , 'volume' , 'create' , '--source' , volume_name, cloned_volume_name]
 
