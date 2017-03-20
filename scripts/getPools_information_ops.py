35060e7560c3dc6feac69ee06f5ed2a4f5f13ffd
OS User <kiran.sridhar@emc.com>
2017-03-20 09:56:49 +0530

    Added New scripts
    Need more documentation in scripts. But these scripts are still
    non-filled up files. Restapi calls need to be added

---
+++ scripts/getPools_information_ops.py
@@ -0,0 +1,13 @@
+import subprocess
+import os
+from json import loads
+from json import dumps
+from pprint import pprint as pp
+import ast
+import time
+import sys
+import re
+
+from ..lib import os_objects
+
+
