9c2c0b3be8747220657091c2c83343c8814108af
OS User <kiran.sridhar@emc.com>
2017-03-21 12:47:41 +0530

    changes_3_21_1247

--- lib/os_objects.py
+++ lib/os_objects.py
@@ -9,15 +9,16 @@ import sys
 import re
 
 class VolumeOperations(object):
-    def __init__(self, **kwargs):
-        for key,value in kwargs.iteritems():
-            print "SOMETHING HERE ? %s = %s" % (key,value)
+    def __init__(self,  bootable_factor, replication_factor, size, **kwargs):
+        self.bootable_factor = bootable_factor
+        self.replication_factor = replication_factor
+        self.size = size
 
+        # for key,value in kwargs.iteritems():
+        #     print "SOMETHING HERE ? %s = %s" % (key,value)
 
-    def volumes_create(self):
-        def __init__(bootable_factor, replication_factor, size):
-            self.bootable_factor = bootable_factor
-            self.replication_factor = replication_factor
-            self.size = size
+    def volumes_create(self,):
+        # def __init__(bootable_factor, replication_factor, size):
+
             print "Requested stuff are %s %s %s" %(self.bootable_factor,self.replication_factor,self.size)
 
