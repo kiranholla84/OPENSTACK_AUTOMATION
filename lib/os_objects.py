3a6b7e8a7e252c773a43a3d51c3112cf9724c0a0
OS User <kiran.sridhar@emc.com>
2017-03-22 15:51:27 +0530

    Volume_creation_function_added

--- lib/os_objects.py
+++ lib/os_objects.py
@@ -7,18 +7,40 @@ import ast
 import time
 import sys
 import re
+import yaml
 
 class VolumeOperations(object):
-    def __init__(self, bootable_factor, replication_factor, size, **kwargs):
+    def __init__(self, bootable_factor, replication_factor, size_vol, type_vol , volume_name, **kwargs):
         self.bootable_factor = bootable_factor
         self.replication_factor = replication_factor
-        self.size = size
+        self.size_vol = size_vol
+        self.type_vol = type_vol
+        self.volume_name = volume_name
 
-        # for key,value in kwargs.iteritems():
-        #     print "SOMETHING HERE ? %s = %s" % (key,value)
+    def latest_volume_status(self):
+        try:
+            op = subprocess.check_output(['openstack', 'volume', 'show', self.volume_name, '-f', 'json'])
+            op = yaml.load(op)
+            return op
+        except subprocess.CalledProcessError as e:
+            print "\nTHERE IS NO VOLUME WITH THE NAME %s" % (self.volume_name)
+            return e.returncode
 
-    def volumes_create(self,):
+    def volumes_create(self):
         # def __init__(bootable_factor, replication_factor, size):
 
-            print "Requested stuff are %s %s %s" %(self.bootable_factor,self.replication_factor,self.size)
+            print "Requested stuff are %s %s %s %s %s " %(self.bootable_factor,self.replication_factor,self.size_vol , self.type_vol , self.volume_name)
+            print "\n================CREATING NON-BOOTABLE VOLUME ================\n"
+            list_checkOutput = ['openstack' ,'volume' ,'create' , '--size', str(self.size_vol) , '--type' , self.type_vol , self.volume_name, '-f','json']
+            op = subprocess.check_output(list_checkOutput)
+            op = loads(op)
+
+            # non-bootable non-attached volume show
+            while (op['status'] != 'available'):
+                if (op['status'].lower == 'error'):
+                    print "\nFAILURE IN CREATING VOLUME %s. EXITING" % (op['name'])
+                    break
+                print "\nWAITING FOR STATUS OF THE VOLUME %s TO BE AVAILABLE. CURRENTLY VOLUME STATE IS IN %s\n" % (op['name'],op['status'])
+                time.sleep(10)
+                op = self.latest_volume_status(self.volume_name)
 
--- scripts/volume_attach_Ops.py
+++ scripts/volume_attach_Ops.py
@@ -11,7 +11,12 @@ import re
 sys.path.insert(0,"../lib")
 from os_objects import *
 
-os_objects_handle = VolumeOperations("nonBootable", "nonReplicated", 5)
+non_rep_vol_type = 'VMAX_SILVER'
+volume_name = 'qe_' + non_rep_vol_type + "2"
+
+os_objects_handle = VolumeOperations("nonBootable", "nonReplicated", 5 , non_rep_vol_type, volume_name)
 non_bootable_volume_list = os_objects_handle.volumes_create()
+
+
 
 
