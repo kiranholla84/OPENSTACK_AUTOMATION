6fef86c4a6adf5edfbc437a68a83a3786703b32a
OS User <kiran.sridhar@emc.com>
2017-03-23 08:24:02 +0530

    changes_8.23_include_variable_time

--- scripts/volume_attach_Ops.py
+++ scripts/volume_attach_Ops.py
@@ -12,7 +12,7 @@ sys.path.insert(0,"../lib")
 from os_objects import *
 
 non_rep_vol_type = 'VMAX_SILVER'
-volume_name = 'qe_' + non_rep_vol_type + "2"
+volume_name = 'qe_' + non_rep_vol_type + '_' + str(time.time())
 
 os_objects_handle = VolumeOperations("nonBootable", "nonReplicated", 5 , non_rep_vol_type, volume_name)
 non_bootable_volume_list = os_objects_handle.volumes_create()
