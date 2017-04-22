1dd2e0bc7e4a9c9d03c1bb6c64b07cc667ba6cab
OS User <kiran.sridhar@emc.com>
2017-04-22 23:02:05 +0530

    22_4_2017
    Create,
    Extend,
    Delete Checked in

--- lib/os_objects.py
+++ lib/os_objects.py
@@ -170,7 +170,6 @@ class VolumeOperations(object):
 
         # Execute the Openstack CLI Command
         op = subprocess.check_output(list_checkOutput)
-
         op = loads(op)
 
         # WAIT FOR ASYNC OPERATION TO COMPLETE
